99c9eb2cb47568d133bbaef0fa8787b4afcaee49
CarlenisVC <yovin1998@icloud.com>
2025-04-03 11:46:10 -0400

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CRUDProyect.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDProyect.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) 
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Models/Entities/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDProyect.Models.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+
+        public string Nombre { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Telefono { get; set; } 
+
+
+
+    }
+}
